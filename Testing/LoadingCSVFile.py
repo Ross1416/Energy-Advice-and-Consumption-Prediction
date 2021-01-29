6b4e317151ea1403feedf5583230488f7c25007e
Ross <r.inglis617@gmail.com>
2021-01-29 13:01:20 +0000

    Made a really basic plot of the dataset. If its right then there seems to be a large bit of unbroken data from mid 2014 to the end of 2014. There might be some small breaks in the data that the graph can't show though.

--- Testing/LoadingCSVFile.py
+++ Testing/LoadingCSVFile.py
@@ -2,5 +2,7 @@ import pandas as pd
 
 data = pd.read_csv("../Energy_Advice_and_Consumption_Prediction_Dataset.csv")
 
+data.columns = ["Date_time", "Total_Feeder"]
+
 print("First 2 lines:")
 print(data.head(2))
