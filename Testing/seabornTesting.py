82a668b63756b777c2fa40b8644a6057b0673b05
Ross <r.inglis617@gmail.com>
2021-02-12 10:17:47 +0000

    tested some of seaborn library

--- Testing/seabornTesting.py
+++ Testing/seabornTesting.py
@@ -1,7 +1,7 @@
+import seaborn
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# data = pd.read_csv("../Energy_Advice_and_Consumption_Prediction_Dataset.csv")  # Read original CSV data file
 data = pd.read_csv("../trainingDataset.csv")  # Read unbroken CSV data file
 
 data.columns = ["Date_time", "Total_Feeder"]  # Rename unnamed column to Data_time
@@ -9,12 +9,5 @@ data.columns = ["Date_time", "Total_Feeder"]  # Rename unnamed column to Data_ti
 data['Date_time'] = pd.to_datetime(data['Date_time'],
                                    format='%Y-%m-%d %H:%M:%S')  # changes the Date_time column to a datetime data type
 
-print("First 2 lines:")
-print(data.head(2))
-
-data.plot(x='Date_time', y='Total_Feeder')  # this plots the graph
-
-
-plt.show()  # shows the plot
-
-print(data.shape)
+seaborn.displot(data['Total_Feeder'])
+plt.show()
