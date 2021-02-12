0e228a5ef8f872792bedd89c594b3fb7c9522df4
Ross <r.inglis617@gmail.com>
2021-02-12 09:59:35 +0000

    updated

--- Testing/PlottingGraph.py
+++ Testing/PlottingGraph.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # data = pd.read_csv("../Energy_Advice_and_Consumption_Prediction_Dataset.csv")  # Read original CSV data file
-data = pd.read_csv("Unbroken_dataset01.csv")  # Read unbroken CSV data file
+data = pd.read_csv("../trainingDataset.csv")  # Read unbroken CSV data file
 
 data.columns = ["Date_time", "Total_Feeder"]  # Rename unnamed column to Data_time
 
@@ -13,6 +13,8 @@ print("First 2 lines:")
 print(data.head(2))
 
 data.plot(x='Date_time', y='Total_Feeder')  # this plots the graph
+
+
 plt.show()  # shows the plot
 
 # If this is right then there seems to be a big chunk of data round about july 2014
