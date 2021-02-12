f2428b0af53a1be49456635c0b96a8cd5699cb7b
Ross <r.inglis617@gmail.com>
2021-02-12 09:47:16 +0000

    updated slightly

--- findingUnbrokenDataFunction.py
+++ findingUnbrokenDataFunction.py
@@ -32,8 +32,21 @@ def findData(fullDatasetPath, outputPath):
 
                 chunkFound = False
 
+        # Testing purposes - To limit how much data is iterated through
+        # count += 1
+        # if count > 10000:
+        #     break
+
+    # Find the start and end date time from the index
+    startDatetime = df.iloc[start]["Date_time"]
+    endDatetime = df.iloc[end]["Date_time"]
+
+    print("Longest unbroken is", longestChunk)  # 4032
+    print("Starting at", startDatetime, start)  # 2015-03-13 00:01:00 115344
+    print("Ending at", endDatetime, end)  # 2015-04-10 00:01:00 119376
+
     outputData = df[start:end]
     outputData.to_csv(outputPath, index=False)
 
 
-findData("../Energy_Advice_and_Consumption_Prediction_Dataset.csv", "dataset.csv")
+findData("../Energy_Advice_and_Consumption_Prediction_Dataset.csv", "trainingDataset.csv")
