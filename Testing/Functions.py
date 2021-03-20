0d61f4ccc575b5c0269dc48b34625090ca2afb1f
Ross <r.inglis617@gmail.com>
2021-03-20 18:41:06 +0000

    updated functions

--- Testing/Functions.py
+++ Testing/Functions.py
@@ -1,7 +1,52 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.svm import SVR
 import pickle
-import numpy as np
+import time
+
+def create_model(dataset_path, output_path):
+    df = pd.read_csv(dataset_path)
+    df.columns = ["Datetime", "Total_Feeder"]
+
+    df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M:%S')  # changes the Date_time column to a datetime data type
+
+    df.set_index('Datetime', inplace=True)
+
+    df["Month"] = df.index.month
+    df["DayofWeek"] = df.index.dayofweek
+    df["DayofMonth"] = df.index.day
+    df["Hour"] = df.index.hour
+    df["Minute"] = df.index.minute
+
+    df.dropna(inplace=True)
+
+    X = df.drop(['Total_Feeder'], axis=1).values
+    y = df['Total_Feeder'].values
+
+    train_split = 0.9
+    num_train = int(len(X) * train_splitu)
+    X_train = X[:num_train]
+    X_test = X[num_train:]
+
+    y_train = y[:num_train]
+    y_test = y[num_train:]
+
+
+    svr = SVR(kernel='rbf', C=40, gamma='auto')
+
+    start_time = time.time()
+
+    svr.fit(X_train, y_train)
+
+    time_taken = time.time() - start_time
+    print("Time taken:", time_taken)
+
+    accuracy = svr.score(X_test, y_test)
+    print(accuracy)  ## only 67%
+
+    with open(output_path, 'wb') as file:
+        pickle.dump(svr, file)
+
 
 def get_model(path):
     with open(path, 'rb') as file:
@@ -22,6 +67,7 @@ def get_predictions(model, date_from, date_to=None):
     df['Predictions'].plot()
     plt.show()
 
+# create_model("..\\Energy_Advice_and_Consumption_Prediction_Dataset.csv", "fullmodel_noYear_noWeek.pickle")
 model = get_model("fullmodel_noYear_noWeek.pickle")
 get_predictions(model, "19/03/2021", "27/03/2021")
 
