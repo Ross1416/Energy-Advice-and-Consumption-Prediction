b4b6f31b48d0d4945dfea2e60af2d60fba45d0ba
Ross <r.inglis617@gmail.com>
2021-03-31 16:02:45 +0100

    final update for Main.py

--- Main.py
+++ Main.py
@@ -7,8 +7,17 @@ import pickle
 import time
 
 
-# FITS MODEL TO DATASET AND SAVES IT
-def CreateModel(dataset_path, output_path=None, type='KNN'):
+# OPEN DATASET AND PERFORM INITITAL FORMATTING
+def GetData(dataset_path):
+    data = pd.read_csv(dataset_path)
+    data.columns = ["Datetime", "Total_Feeder"]
+    data['Datetime'] = pd.to_datetime(data['Datetime'], format='%Y-%m-%d %H:%M:%S')
+    data.set_index('Datetime', inplace=True)
+    return data
+
+
+# FIT MODEL TO DATASET AND SAVE IT
+def CreateModel(data, output_path=None, type='KNN'):
 
     # Create Regressor model
     if type == "KNN":
@@ -20,19 +29,13 @@ def CreateModel(dataset_path, output_path=None, type='KNN'):
     else:
         print("Not a valid regression algorithm.")
 
-    # Read in dataset and perform preliminary sorting
-    df = pd.read_csv(dataset_path)
-    df.columns = ["Datetime", "Total_Feeder"]
-    df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M:%S')
-    df.set_index('Datetime', inplace=True)
+    # Copy data to new dataframe
+    df = data.copy()
 
     # Split the datetime into multiple individual columns
-    # df["Year"] = df.index.year
     df["Month"] = df.index.month
     df["DayofWeek"] = df.index.dayofweek
     df["DayofMonth"] = df.index.day
-    # df["DayOfYear"] = df.index.dayofyear
-    # df["Week"] = df.index.week
     df["Hour"] = df.index.hour
     df["Minute"] = df.index.minute
 
@@ -43,7 +46,7 @@ def CreateModel(dataset_path, output_path=None, type='KNN'):
     X = df.drop(['Total_Feeder'], axis=1).values
     y = df['Total_Feeder'].values
 
-    train_split = 0.9   ##   MIGHT BE LOSING SOME DATA WITH THIS METHOD? better way to do it?### https://medium.com/@alexstrebeck/training-and-testing-machine-learning-models-e1f27dc9b3cb
+    train_split = 0.9
     num_train = int(len(X) * train_split)
     X_train = X[:num_train]
     X_test = X[num_train:]
@@ -51,22 +54,23 @@ def CreateModel(dataset_path, output_path=None, type='KNN'):
     y_train = y[:num_train]
     y_test = y[num_train:]
 
-
     start_time = time.time()
 
     # Fit model to data
     model.fit(X_train, y_train)
 
+    # Find and output the time taken to train                   # Time taken
     time_taken = time.time() - start_time                       # RBF                           # RFR                       #KNN
     print("Time taken:", time_taken, "seconds")                 # 2402.46537232399 seconds      3.545670509338379           0.391770601272583
 
-    accuracy_train = model.score(X_train, y_train)
-    accuracy_test = model.score(X_test, y_test)
-    print("Training accuracy:", accuracy_train)                  # 0.9447776916752073            0.7372641404547604          0.9403859188290737
-    print("Testing accuracy:", accuracy_test)                    # 0.6614105265554282            0.77431329514064            0.8014318330506364
+    # Calculate and output the accuracies
+    score_train = model.score(X_train, y_train)
+    score_test = model.score(X_test, y_test)                    # Accuracy
+    print("Training score:", score_train)                       # 0.9447776916752073            0.7372641404547604          0.9403859188290737
+    print("Testing score:", score_test)                         # 0.6614105265554282            0.77431329514064            0.8014318330506364
 
+    # Save the model to external file if path is provided
     if output_path is not None:
-        # Save the model to external file
         with open(output_path, 'wb') as file:
             pickle.dump(model, file)
 
@@ -115,14 +119,14 @@ def MakePredictions(model, start_date, end_date, save=False, save_folder=None):
 # PLOTS PREDICTIONS ON GRAPH
 def PlotPredictions(predictions, save=False, save_folder=None, actuals=None):
     # Plot predictions on a graph
-    ax = predictions.plot()
+    ax = predictions.plot(zorder=10, color="#ff7f0e")
 
     # Plot actual values on graph if actuals is provided and within the date range
     if actuals is not None:
         try:
             actuals = actuals.loc[predictions.first_valid_index():predictions.last_valid_index()]
             actuals.columns = ["Actual"]
-            actuals.plot(ax=ax)
+            actuals.plot(ax=ax, zorder=1, color="#1f77b4")
         except:
             print("The date time range is not within the dataset.")
 
@@ -148,15 +152,9 @@ def PlotPredictions(predictions, save=False, save_folder=None, actuals=None):
     plt.show()
 
 
-model = CreateModel("Energy_Advice_and_Consumption_Prediction_Dataset.csv", type="SVR") # "Models/RFR_Month_DoW_DoM_Hour_Min.pickle"
-# model = GetModel("Models/SVR_Month_DoW_DoM_Hour_Min.pickle")
-df = MakePredictions(model, "02/03/2015", "03/03/2015", save=False, save_folder="Predictions")
-#
-data = pd.read_csv("Energy_Advice_and_Consumption_Prediction_Dataset.csv")
-data.columns = ["Datetime", "Total_Feeder"]
-data.columns = ["Datetime", "Total_Feeder"]
-data['Datetime'] = pd.to_datetime(data['Datetime'], format='%Y-%m-%d %H:%M:%S')
-data.set_index('Datetime', inplace=True)
-#
-PlotPredictions(df, save=False, save_folder="Predictions", actuals=data)
-# # PlotPredictions(df, save=True, save_folder="Predictions")
+data = GetData("Energy_Advice_and_Consumption_Prediction_Dataset.csv")
+# model = CreateModel(data, "Models/test.pickle", type="KNN")
+model = GetModel("Models/KNN_Month_DoW_DoM_Hour_Min.pickle")
+predictions = MakePredictions(model, "01/03/2015", "02/03/2015", save=False, save_folder="Predictions")
+PlotPredictions(predictions, save=False, save_folder="Predictions", actuals=data)
+
